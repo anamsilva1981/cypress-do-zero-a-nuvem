ff31ddeb5d7db01fd642961d5172be3b5a12c85d
anamsilva1981 <ana.msilva@hotmail.com>
2025-01-23 09:42:52 -0300

    exibe mensagem de erro ao submeter o formulário com um email com formatação inválida

--- cypress/e2e/cac-tat.cy.js
+++ cypress/e2e/cac-tat.cy.js
@@ -12,7 +12,7 @@ describe('Central de Atendimento ao Cliente TAT', () => {
 
   it('Preenche os campos obrigatórios e envia o formulário', () => {
 
-    const longText = Cypress._.repeat('texto que sera repetido', 10)
+    // const longText = Cypress._.repeat('texto que sera repetido', 10)
     // Preenchimento do formulario
     cy.get('#firstName').type('Valmir')
     cy.get('#lastName').type('Soriano')
@@ -21,10 +21,22 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     // cy.get('#open-text-area').type(longText, {delay: 0}) 
     // utiliza a variavel longText, no lugar de textos muito longos
     // {delay: 0} -> Determina o delay na execução de 0 a 10.
-    cy.get('#open-text-area').type('Texto teste para preenchimento do campo de texto ') -> forma normal
+    cy.get('#open-text-area').type('Texto teste para preenchimento do campo de texto ') 
     cy.get('button[type="submit"]').click()
 
     // Verifica o retorno do formulário enviado 
     cy.get('.success').should('be.visible')
+  })
+
+  it.only('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', () => {
+    cy.get('#firstName').type('Valmir')
+    cy.get('#lastName').type('Soriano')
+    cy.get('#email').type('teste@teste.com,br')
+    cy.get('#phone').type('11000000000')
+    cy.get('#open-text-area').type('Texto teste para preenchimento do campo de texto ') 
+    cy.get('button[type="submit"]').click()
+
+    // Verifica o retorno do formulário enviado 
+    cy.get('.error').should('be.visible')
   })
 })
