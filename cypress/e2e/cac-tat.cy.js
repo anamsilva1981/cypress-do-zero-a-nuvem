ebd0ff75fe6373ccf6321ed2591c390cf3c72096
anamsilva1981 <ana.msilva@hotmail.com>
2025-01-23 10:06:48 -0300

    exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário

--- cypress/e2e/cac-tat.cy.js
+++ cypress/e2e/cac-tat.cy.js
@@ -40,9 +40,23 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.get('.error').should('be.visible')
   })
 
-  it.only('valida se um campo continua vazio se digitado por um valor não numérico', ()=> {
+  it('valida se um campo continua vazio se digitado por um valor não numérico', ()=> {
     cy.get('#phone')
       .type('abcd')
       .should('have.value', '')
+  })
+
+  it.only('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', () => {
+    //Ações
+    cy.get('#firstName').type('Valmir')
+    cy.get('#lastName').type('Soriano')
+    cy.get('#email').type('teste@teste.com,br')
+    // cy.get('#phone').type('11000000000')
+    cy.get('#open-text-area').type('Texto teste para preenchimento do campo de texto ') 
+    cy.get('#phone-checkbox').click()
+    cy.get('button[type="submit"]').click()
+
+    // Resultado esperado 
+    cy.get('.error').should('be.visible')
   })
 })
