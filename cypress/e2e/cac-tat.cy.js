95dfe0c1b7f4962a7b7f2924026bdac3e7a78b2f
anamsilva1981 <ana.msilva@hotmail.com>
2025-01-23 09:32:58 -0300

    Adiciona delay para determinar o tempo de escrita da aplicação

--- cypress/e2e/cac-tat.cy.js
+++ cypress/e2e/cac-tat.cy.js
@@ -18,7 +18,9 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.get('#lastName').type('Soriano')
     cy.get('#email').type('teste@teste.com.br')
     cy.get('#phone').type('11000000000')
-    cy.get('#open-text-area').type(longText) // utilizado em textos muito longos
+    cy.get('#open-text-area').type(longText, {delay: 0}) 
+    // utiliza a variavel longText, no lugar de textos muito longos
+    // {delay: 0} -> Determina o delay na execução de 0 a 10.
     // cy.get('#open-text-area').type('Texto teste para preenchimento do campo de texto ') -> forma normal
     cy.get('button[type="submit"]').click()
 
